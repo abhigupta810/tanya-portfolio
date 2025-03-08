66da1f4e6e36b175b5538c6db56d92ba47fd60eb
abhigupta810 <155122504+abhigupta810@users.noreply.github.com>
2025-03-08 21:19:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
